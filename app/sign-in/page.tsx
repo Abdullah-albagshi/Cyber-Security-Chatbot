aebd01486acc99e884d62b6593fb7cb0c3838228
Abdullah albagshi <abdullah.albagshi@gmail.com>
2023-12-31 21:10:37 +0300

    Refactor sign-in page and update password button color

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,5 +1,6 @@
-import { auth } from '@/auth'
+import LandingPage from '@/components/ui/LandingPage'
 import { LoginButton } from '@/components/login-button'
+import { auth } from '@/auth'
 import { redirect } from 'next/navigation'
 
 export default async function SignInPage() {
@@ -9,8 +10,8 @@ export default async function SignInPage() {
     redirect('/')
   }
   return (
-    <div className="flex h-[calc(100vh-theme(spacing.16))] items-center justify-center py-10">
-      <LoginButton />
+    <div className="flex flex-col items-center justify-center w-full h-full px-4 py-16 space-y-8">
+      <LandingPage/>
     </div>
   )
 }
--- components/PasswordPage.tsx
+++ components/PasswordPage.tsx
@@ -73,7 +73,9 @@ const PasswordPage = () => {
         </div>
         <button
           onClick={() => generatePassword()}
-          className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg"
+          // className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg"
+          // green button
+          className="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg"
         >
           Generate Password
         </button>
