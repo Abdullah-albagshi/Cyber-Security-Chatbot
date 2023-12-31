ef333ac0cd50e47e10bde226108c6f80f2aa82f4
Abdullah albagshi <abdullah.albagshi@gmail.com>
2023-12-31 20:35:25 +0300

    Update chatbot introduction message

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -31,7 +31,7 @@ export function EmptyScreen({ setInput }: Pick<UseChatHelpers, 'setInput'>) {
     <div className="mx-auto max-w-2xl px-4">
       <div className="rounded-lg border bg-background p-8">
         <h2 className="text-2xl font-bold mb-4">Hello! I am an advanced chatbot specializing in cyber security.</h2>
-        <p className="mb-4">My goal is to help you create a strong and reliable cybersecurity plan for your company. With your input on data storage and sharing practices, I can offer expert guidance on developing an effective strategy. I'll provide recommendations on encryption methods, firewall creation, and policies to safeguard against suspicious activities and potential threats.</p>
+        <p className="mb-4">{"My goal is to help you create a strong and reliable cybersecurity plan for your company. With your input on data storage and sharing practices, I can offer expert guidance on developing an effective strategy. I'll provide recommendations on encryption methods, firewall creation, and policies to safeguard against suspicious activities and potential threats."}</p>
         <p className="mb-4">How can I assist you in building a robust cybersecurity plan for your company?</p>
         <div className="flex flex-col space-y-4">
           {exampleMessages.map(({ heading, message }) => (
